4f9ec3cec9df81374548cb1c26b7f13ba6cd279f
AKU CANTIK 💃 <hozooapi@gmail.com>
2025-02-23 14:57:38 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -5,4 +5,16 @@ echo -e "🥷 PENGINSTALAN MODULE PYTHON"
 sleep 2
 raed -p " enter "
 echo -e $read" SEDANG MENGINSTALL"
+pkg install curl -y
+clear
+pkg install jq -y
+clear
+pkg install wger -y
+clear
+pkg install git -y
+clear
+pkg install bash -y
+clear
 pip install -r install.txt
+
+python pap.py
